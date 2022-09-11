60bdc1f426792d626acea2b5df90711aeb19efc9
Made Swastika Nata Negara <swastika.nata1804@gmail.com>
2022-09-11 16:58:39 +0700

    Membuat app katalog

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'list_item': data_catalog_item,
+        'name': 'Made Swastika Nata Negara',
+        'id': '2106630095',
+    }
+
+    return render(request, "katalog.html", context)
