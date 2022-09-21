5ddb7cf30f487fd9fee5110ff30194f42b990d5c
Made Swastika Nata Negara <swastika.nata1804@gmail.com>
2022-09-21 12:45:38 +0700

    assignment3: delete mywatchlist in models

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -1,12 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-class MyWatchList(models.Model):
-    watched = models.BooleanField()
-    title = models.CharField(max_length=255)
-    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
-    release_date = models.CharField(max_length=255)
-    review = models.TextField()
+
 
 class MyWatchListModel(models.Model):
     watched = models.BooleanField()
